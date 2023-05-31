f823ccd90b80c25604376c0ac8c16ba455748d7b
Toh Chen Long <chenlongtoh@gmail.com>
2023-05-31 12:30:40 +0800

    Initial Commit

---
+++ MovieApp/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Lumos - Toh Chen Long on 31/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
